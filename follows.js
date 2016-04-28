588ef1b9917b4246b91495742dca9eaee6f338df
olafzanger <olaf@MBA-Olaf.fritz.box>
2016-04-28 14:54:19 +0200

    make it mine

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'PWFJlfKukLbKIr0ja2I5yI2ymPGfR1qD3yTxIKlx0O2teyK6', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [968086980], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Rural Solar Innovation', // Replaces Xively logo in the header
+		dataDuration	= 'year', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0000FF', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
